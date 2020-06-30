873915d305025ab3934c8254457b2b09e3ee831b
carminelentisco <carminelentisco@gmail.com>
2020-06-30 15:40:26 +0200

    Modifiche allo style admin/show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,39 +1,35 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h1 class="mb-4">{{ $post->title }}</h1>
+    <div class="container d-flex flex-column align-items-center">
+        <h1 class="mb-4">Il tuo post</h1>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Titolo</th>
-                    <th>Testo</th>
-                    <th>Creato il</th>
-                    <th>Aggiornato il</th>
-                    <th colspan="4"></th>
-                </tr>
-            </thead>
-            <tbody>
-                    <tr>
-                        <td>{{ $post->id }}</td>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->body }}</td>
-                        <td>{{ $post->created_at }}</td>
-                        <td>{{ $post->updated_at }}</td>
-                        <td>
-                            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-primary">Modifica</a>
-                        </td>
-                        <td>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    @if (!empty($post->path_img)) 
+                        <img height="100%" class="card-img-top" src="{{ asset('/storage/' . $post->path_img) }}" alt="{{ $post->title }}">
+                    @else
+                        <div class="alert alert-info">
+                            <h2>Non Vi sono immagini disponibili</h2>
+                        </div>
+                    @endif
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>{{ $post->title }}</strong></h5>
+                        <p class="card-text">{{ $post->body }}</p>
+                        <div class="d-flex justify-content-around">
+                            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-dark">Modifica</a>
                             <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <input class="btn btn-danger" type="submit" value="Elimina">
                             </form>
-                        </td>
-                    </tr>
-            </tbody>
-        </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
